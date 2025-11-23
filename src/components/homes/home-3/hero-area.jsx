d76124dc9a760704996391a66113d171294ed3fe
eswasai <eswarharshith@gmail.com>
2025-11-24 02:17:52 +0530

    Update hero-area.jsx

--- src/components/homes/home-3/hero-area.jsx
+++ src/components/homes/home-3/hero-area.jsx
@@ -13,7 +13,7 @@ import Link from "next/link";
 const hero_content = {
   title_1: (
     <>
-      Scale <span>Customers</span>
+      Scales <span>Customers</span>
     </>
   ),
   title_2: "Capture Success.",
